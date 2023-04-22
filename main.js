bee8f4035d54cede49facd7b9995e79b73289d5c
Fernando Souza <114751334+fherexe@users.noreply.github.com>
2023-04-22 15:43:12 -0300

    att

--- main.js
+++ main.js
@@ -10,3 +10,53 @@ function aumentar(){
   tamanho++;
   document.body.style.fontSize=tamanho+"px";
 }
+
+
+//Menu de navegação
+var content1 = document.getElementById("caixa1");
+var content2 = document.getElementById("caixa2");
+var content3 = document.getElementById("caixa3");
+var content4 = document.getElementById("caixa4");
+
+function mostrar1() {
+	content1.style.display = "block";
+	content2.style.display = "none";
+	content3.style.display = "none";
+	content4.style.display = "none";
+}
+
+function mostrar2() {
+	if (content2.style.display === "none") {
+		content1.style.display = "none";
+    content2.style.display = "block";
+		content3.style.display = "none";
+		content4.style.display = "none";
+	}
+}
+
+function mostrar3() {
+	if (content3.style.display === "none") {
+		content1.style.display = "none";
+    content2.style.display = "none";
+		content3.style.display = "block";
+		content4.style.display = "none";
+	}
+}
+
+function mostrar4() {
+	if (content4.style.display === "none") {
+		content1.style.display = "none";
+    content2.style.display = "none";
+		content3.style.display = "none";
+		content4.style.display = "block";
+	}
+}
+
+
+
+
+
+
+
+
+
