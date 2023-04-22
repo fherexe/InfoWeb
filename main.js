c4924d294753a5af5e92b8294a83f504ed702392
Fernando Souza <114751334+fherexe@users.noreply.github.com>
2023-04-22 16:17:25 -0300

    att

--- main.js
+++ main.js
@@ -4,58 +4,34 @@ var content2 = document.getElementById("caixa2");
 var content3 = document.getElementById("caixa3");
 var content4 = document.getElementById("caixa4");
 
-
 function mostrar1() {
-	if (content1.style.display === "none"){
-		content1.style.display = "block";
-		content2.style.display = "none";
-		content3.style.display = "none";
-		content4.style.display = "none";
-	}
- 	else {
-		content1.style.display = "block";
-	}
+	content1.style.display = "block";
+	content2.style.display = "none";
+	content3.style.display = "none";
+	content4.style.display = "none";
 }
 
 function mostrar2() {
-	if (content2.style.display === "none") {
-		content1.style.display = "none";
-    	content2.style.display = "block";
-		content3.style.display = "none";
-		content4.style.display = "none";
-	}
-
-	else {
-		content2.style.display = "block";
-	}
+	content1.style.display = "none";
+    content2.style.display = "block";
+	content3.style.display = "none";
+	content4.style.display = "none";
 }
 
 function mostrar3() {
-	if (content3.style.display === "none") {
-		content1.style.display = "none";
-    	content2.style.display = "none";
-		content3.style.display = "block";
-		content4.style.display = "none";
-	}
-
-	else {
-		content3.style.display = "block";
-	}
+	content1.style.display = "none";
+    content2.style.display = "none";
+	content3.style.display = "block";
+	content4.style.display = "none";
 }
+
 
 function mostrar4() {
-	if (content4.style.display === "none") {
-		content1.style.display = "none";
-    	content2.style.display = "none";
-		content3.style.display = "none";
-		content4.style.display = "block";
-	}
-
-	else {
-		content4.style.display = "block";
-	}
+	content1.style.display = "none";
+    content2.style.display = "none";
+	content3.style.display = "none";
+	content4.style.display = "block";
 }
-
 
 //Recurso de alterar tamanho das fontes
 var tamanho = 16;
