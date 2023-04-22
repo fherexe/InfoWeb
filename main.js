784ad18da05ed079f8a4edcd61287e224cbd0996
Fernando Souza <114751334+fherexe@users.noreply.github.com>
2023-04-22 16:06:45 -0300

    att

--- main.js
+++ main.js
@@ -1,3 +1,62 @@
+//Menu de navegação
+var content1 = document.getElementById("caixa1");
+var content2 = document.getElementById("caixa2");
+var content3 = document.getElementById("caixa3");
+var content4 = document.getElementById("caixa4");
+
+
+function mostrar1() {
+	if (content1.style.display === "none"){
+		content1.style.display = "block";
+		content2.style.display = "none";
+		content3.style.display = "none";
+		content4.style.display = "none";
+	}
+ 	else {
+		content1.style.display = "block";
+	}
+}
+
+function mostrar2() {
+	if (content2.style.display === "none") {
+		content1.style.display = "none";
+    	content2.style.display = "block";
+		content3.style.display = "none";
+		content4.style.display = "none";
+	}
+
+	else {
+		content2.style.display = "block";
+	}
+}
+
+function mostrar3() {
+	if (content3.style.display === "none") {
+		content1.style.display = "none";
+    	content2.style.display = "none";
+		content3.style.display = "block";
+		content4.style.display = "none";
+	}
+
+	else {
+		content3.style.display = "block";
+	}
+}
+
+function mostrar4() {
+	if (content4.style.display === "none") {
+		content1.style.display = "none";
+    	content2.style.display = "none";
+		content3.style.display = "none";
+		content4.style.display = "block";
+	}
+
+	else {
+		content4.style.display = "block";
+	}
+}
+
+
 //Recurso de alterar tamanho das fontes
 var tamanho = 16;
 
@@ -12,45 +71,6 @@ function aumentar(){
 }
 
 
-//Menu de navegação
-var content1 = document.getElementById("caixa1");
-var content2 = document.getElementById("caixa2");
-var content3 = document.getElementById("caixa3");
-var content4 = document.getElementById("caixa4");
-
-function mostrar1() {
-	content1.style.display = "block";
-	content2.style.display = "none";
-	content3.style.display = "none";
-	content4.style.display = "none";
-}
-
-function mostrar2() {
-	if (content2.style.display === "none") {
-		content1.style.display = "none";
-    content2.style.display = "block";
-		content3.style.display = "none";
-		content4.style.display = "none";
-	}
-}
-
-function mostrar3() {
-	if (content3.style.display === "none") {
-		content1.style.display = "none";
-    content2.style.display = "none";
-		content3.style.display = "block";
-		content4.style.display = "none";
-	}
-}
-
-function mostrar4() {
-	if (content4.style.display === "none") {
-		content1.style.display = "none";
-    content2.style.display = "none";
-		content3.style.display = "none";
-		content4.style.display = "block";
-	}
-}
 
 
 
